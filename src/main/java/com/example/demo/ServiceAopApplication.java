7027fdb394f39c7be423f6c8375f5dd59e1c824d
MVP Java <mvpjava.email@gmail.com>
2020-09-28 00:00:12 +0000

    new project

---
+++ src/main/java/com/example/demo/ServiceAopApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceAopApplication.class, args);
+	}
+
+}
