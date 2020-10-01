659c0627b9e49d58be9a9b09bd2f16c07b96b2c5
MVP Java <blog@mvpjava.com>
2020-10-01 16:04:39 +0000

    cleanup

--- src/main/java/com/mvpjava/aop/ServiceVsComponentAspect.java
+++ src/main/java/com/mvpjava/aop/ServiceVsComponentAspect.java
@@ -13,32 +13,8 @@ public class ServiceVsComponentAspect {
 
 	private Logger logger = LoggerFactory.getLogger(getClass());
 
-	// 1. match the DiscountGeneratorService and BootstrapCommandLineRunner and
-	// possible future additions of @Component thus we need to refine
-	// @Before("@within(org.springframework.stereotype.Component)")
-	
-	// 2. more specific (harder to read, what is * again?)
-	// @Before("execution(double calculateDiscountFor(*)) &&  @within(org.springframework.stereotype.Component)")
-	// or (but more brittle since if you move Product to another package or
-	// rename method or change return type to BigDecimal then it will break pointcut
-	// @Before("execution(double calculateDiscountFor(com.mvpjava.demo.Product)) &&  @within(org.springframework.stereotype.Component)")
-
-	//@Before("@within(com.mvpjava.demo.TransactionalService)")
 	@Before("@within(org.springframework.stereotype.Service)")
 	public void implLogging(JoinPoint joinPoint) {
 		logger.info(" Around advice matched Class [" + joinPoint.getSignature().toLongString() + "]");
 	}
-
-
-// This reads better
-//
-//	@Before("allMethodsWithServiceAnnotationOnClassPointcut()")
-//	public void implLogging(JoinPoint joinPoint) {
-//        logger.info(" advice implementation - " + joinPoint.getTarget().getClass() + //
-//                    "; Executing before " + joinPoint.getSignature().getName() + //
-//                    "() method");
-//	}
-//	
-//	@Pointcut("@within(org.springframework.stereotype.Service)")
-//	public void allMethodsWithServiceAnnotationOnClassPointcut(){}
 }
